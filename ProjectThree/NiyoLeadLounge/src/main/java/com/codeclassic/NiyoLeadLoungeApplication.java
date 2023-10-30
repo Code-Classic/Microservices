a118d010f618ac68dd191cc27966eea32dc50477
AhteShaikh <121925461+AhteShaikh@users.noreply.github.com>
2023-10-30 16:15:30 +0530

    starting crm in project three

---
+++ ProjectThree/NiyoLeadLounge/src/main/java/com/codeclassic/NiyoLeadLoungeApplication.java
@@ -0,0 +1,13 @@
+package com.codeclassic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiyoLeadLoungeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiyoLeadLoungeApplication.class, args);
+	}
+
+}
---
+++ ProjectThree/NiyoLeadLounge/src/main/java/com/codeclassic/entity/Employee.java
@@ -0,0 +1,5 @@
+package com.codeclassic.entity;
+
+public class Employee {
+	
+}
